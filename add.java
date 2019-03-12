6a072bd3546de04f62819e6d6ed13ffea13fb4c2
manisha141 <manishamspillai@97>
2019-03-12 11:23:21 +0530

    My second java pgm

---
+++ add.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class add{
+   public static void main(String[]args) {
+    Scanner myinput=new Scanner(System.in);
+    int a,b,c;
+    System.out.println("Enter 1st number");
+    a=myinput.nextInt();
+    System.out.println("Enter 2nd number");
+    b=myinput.nextInt();
+    c=a+b;
+    System.out.println(c);
+
+     
+
+   
+    }
+}
